a0270dab285ae8db2545ae3d451cb69e79a8f5e7
wilson <binaryaoe@163.com>
2022-03-27 23:18:21 +0800

    feat: init rust project

---
+++ src/print.rs
@@ -0,0 +1,34 @@
+pub fn run() {
+    // Print to console
+    println!("Hello from the print file");
+
+    // Basic Formatting
+    println!("Number {}", 1);
+
+    println!("{} + {} = {}", 1, 1, 2);
+
+    // Positional Arguments
+    println!(
+        "{0} is from {1} and {0} loves {2}",
+        "Wilson", "mass", "coding"
+    );
+
+    // Named Arguments
+    println!(
+        "{name} likes {hobby}",
+        name = "Wilson",
+        hobby = "running"
+    );
+
+    // Placeholder traits
+    println!(
+        "Binary: {:b}, Hex: {:x}, Octal: {:o}",
+        10, 10, 10
+    );
+
+    // Placeholder for debug trait
+    println!(
+        "{:?}",
+        (12, true, "hello")
+    );
+}
